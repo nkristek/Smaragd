c2589857603db7bcb1ccb21776c8ee4b9219e568
Niclas Kristek <nkristek@users.noreply.github.com>
2018-03-21 22:35:19 +0100

    Increase version to 1.1

--- MVVMBase/Properties/AssemblyInfo.cs
+++ MVVMBase/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("nkristek.MVVMBase")]
-[assembly: AssemblyDescription("Base classes for implementing an application using the MVVM architecture")]
+[assembly: AssemblyDescription("Base classes for implementing an C# .NET application using the MVVM architecture")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("nkristek")]
 [assembly: AssemblyProduct("MVVMBase")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
