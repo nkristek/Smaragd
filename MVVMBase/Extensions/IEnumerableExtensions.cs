56aa8c56ef6d31a0b741b4bc94b7fb2316000b5c
Niclas Kristek <nkristek@users.noreply.github.com>
2018-03-21 21:34:51 +0100

    Implement a MergeSequence method on IEnumerable<T> to merge two sequences

--- MVVMBase/Extensions/IEnumerableExtensions.cs
+++ MVVMBase/Extensions/IEnumerableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace nkristek.MVVMBase.Extensions
 {
@@ -15,6 +16,55 @@ namespace nkristek.MVVMBase.Extensions
         {
             foreach (var item in enumeration)
                 action(item);
+        }
+
+        /// <summary>
+        /// This method merges two sequences.
+        /// It works like <see cref="IEnumerable{T}.Union(IEnumerable{T}).Distinct()"/>, but also tries to preserve the order of the secondary sequence.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="primarySequence">Primary sequence which will be prioritzed when conflicts occur</param>
+        /// <param name="secondarySequence">Secondary sequence</param>
+        /// <param name="equalsPropertyAccessor">This function should return a property which is used to differentiate between the <see cref="T"/> instances.</param>
+        /// <returns></returns>
+        public static IEnumerable<T> MergeSequence<T>(this IEnumerable<T> primarySequence, IEnumerable<T> secondarySequence, Func<T, object> equalsPropertyAccessor = null)
+        {
+            if (primarySequence == null)
+                throw new ArgumentNullException("primarySequence");
+            if (secondarySequence == null)
+                throw new ArgumentNullException("secondarySequence");
+            if (equalsPropertyAccessor == null)
+                equalsPropertyAccessor = arg => arg;
+
+            var primaryItems = primarySequence.ToList();
+            var notContainedItems = new List<T>();
+
+            foreach (var secondaryItem in secondarySequence)
+            {
+                var secondaryItemProperty = equalsPropertyAccessor(secondaryItem);
+                var indexOfPrimaryItem = primaryItems.FindIndex(item =>
+                {
+                    var itemProperty = equalsPropertyAccessor(item);
+                    if (itemProperty != null)
+                        return itemProperty.Equals(secondaryItemProperty);
+                    return secondaryItemProperty == null;
+                });
+
+                var secondaryItemExistsInPrimaryItems = indexOfPrimaryItem >= 0;
+                if (secondaryItemExistsInPrimaryItems)
+                {
+                    // insert notContainedItems before this item
+                    primaryItems.InsertRange(indexOfPrimaryItem, notContainedItems);
+                    notContainedItems.Clear();
+                }
+                else
+                {
+                    notContainedItems.Add(secondaryItem);
+                }
+            }
+            primaryItems.AddRange(notContainedItems);
+
+            return primaryItems;
         }
     }
 }
--- MVVMBaseTests/ExtensionsTests.cs
+++ MVVMBaseTests/ExtensionsTests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using nkristek.MVVMBase.Extensions;
 
@@ -33,6 +35,25 @@ namespace nkristek.MVVMBaseTest
 
             var validFileName = invalidFileName.ReplaceInvalidFileNameChars('a');
             Assert.IsFalse(validFileName.ContainsInvalidFileNameChars());
+        }
+
+        [TestMethod]
+        public void TestMergeSequence()
+        {
+            var firstSequence = new List<string> { "1", "3" };
+            var secondSequence = new List<string> { "1", "2", "3" };
+            var mergedSequence = firstSequence.MergeSequence(secondSequence);
+            Assert.IsTrue(mergedSequence.SequenceEqual(new List<string> { "1", "2", "3" }));
+
+            firstSequence = new List<string> { "1", "2", "4" };
+            secondSequence = new List<string> { "2", "3", "4", "5" };
+            mergedSequence = firstSequence.MergeSequence(secondSequence);
+            Assert.IsTrue(mergedSequence.SequenceEqual(new List<string> { "1", "2", "3", "4", "5" }));
+
+            firstSequence = new List<string> { "1", "2", "3" };
+            secondSequence = new List<string> { "4", "3", "2", "1" };
+            mergedSequence = firstSequence.MergeSequence(secondSequence);
+            Assert.IsTrue(mergedSequence.SequenceEqual(new List<string> { "1", "2", "4", "3" }));
         }
     }
 }
