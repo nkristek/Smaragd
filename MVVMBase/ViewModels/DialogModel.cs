fa1e4b2d479e631c6592c38fd250c48ca9aaa124
Niclas Kristek <nkristek@users.noreply.github.com>
2018-04-14 17:00:15 +0200

    Implement a DialogModel

---
+++ MVVMBase/ViewModels/DialogModel.cs
@@ -0,0 +1,15 @@
+﻿namespace nkristek.MVVMBase.ViewModels
+{
+    public abstract class DialogModel : ViewModel
+    {
+        private string _Title;
+        /// <summary>
+        /// Dialog title
+        /// </summary>
+        public string Title
+        {
+            get { return _Title; }
+            set { SetProperty(ref _Title, value); }
+        }
+    }
+}
