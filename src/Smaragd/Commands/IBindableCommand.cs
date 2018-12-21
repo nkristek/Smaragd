1691731d25e4e4d259b04dc25c7a9f76c2f42d70
Niclas Kristek <nkristek@users.noreply.github.com>
2018-12-21 14:38:10 +0100

    Add IBindableCommand interface

---
+++ src/Smaragd/Commands/IBindableCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Input;
+using NKristek.Smaragd.ViewModels;
+
+namespace NKristek.Smaragd.Commands
+{
+    /// <inheritdoc cref="ICommand" />
+    /// <summary>
+    /// Defines a command with <see cref="T:System.ComponentModel.INotifyPropertyChanging" /> and <see cref="T:System.ComponentModel.INotifyPropertyChanged" /> support.
+    /// </summary>
+    public interface IBindableCommand
+        : ICommand, IRaisePropertyChanging, IRaisePropertyChanged
+    {
+    }
+}
