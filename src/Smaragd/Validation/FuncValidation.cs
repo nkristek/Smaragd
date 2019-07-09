384237f8e36894a01169127299ad8c7bfb6849b8
Niclas Kristek <nkristek@users.noreply.github.com>
2019-07-09 16:44:19 +0200

    Rename generic result type of validations to TResult

--- src/Smaragd/Validation/FuncValidation.cs
+++ src/Smaragd/Validation/FuncValidation.cs
@@ -6,10 +6,10 @@ namespace NKristek.Smaragd.Validation
     /// <remarks>
     /// Validates a given value using a function.
     /// </remarks>
-    public sealed class FuncValidation<TValue, TValidationResult>
-        : IValidation<TValue, TValidationResult>
+    public sealed class FuncValidation<TValue, TResult>
+        : IValidation<TValue, TResult>
     {
-        private readonly Func<TValue, TValidationResult> _validationFunc;
+        private readonly Func<TValue, TResult> _validationFunc;
 
         /// <inheritdoc />
         /// <summary>
@@ -17,13 +17,13 @@ namespace NKristek.Smaragd.Validation
         /// </summary>
         /// <param name="validationFunc">A <see cref="Func{T, TResult}"/> which is used to validate the value.</param>
         /// <exception cref="ArgumentNullException"><paramref name="validationFunc"/> is <see langword="null"/>.</exception>
-        public FuncValidation(Func<TValue, TValidationResult> validationFunc)
+        public FuncValidation(Func<TValue, TResult> validationFunc)
         {
             _validationFunc = validationFunc ?? throw new ArgumentNullException(nameof(validationFunc));
         }
 
         /// <inheritdoc />
-        public TValidationResult Validate(TValue value)
+        public TResult Validate(TValue value)
         {
             return _validationFunc(value);
         }
--- src/Smaragd/Validation/IValidation.cs
+++ src/Smaragd/Validation/IValidation.cs
@@ -4,14 +4,14 @@
     /// Validates a given value.
     /// </summary>
     /// <typeparam name="TValue">The type of the value to validate.</typeparam>
-    /// <typeparam name="TValidationResult">The type of the result.</typeparam>
-    public interface IValidation<in TValue, out TValidationResult>
+    /// <typeparam name="TResult">The type of the result.</typeparam>
+    public interface IValidation<in TValue, out TResult>
     {
         /// <summary>
         /// Validates the given <paramref name="value"/>.
         /// </summary>
         /// <param name="value">The value to validate.</param>
         /// <returns>The result of the validation.</returns>
-        TValidationResult Validate(TValue value);
+        TResult Validate(TValue value);
     }
 }
