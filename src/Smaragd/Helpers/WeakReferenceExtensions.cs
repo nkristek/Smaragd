ab69104550141772e808f5bb492b63494c57fe3c
Niclas Kristek <nkristek@users.noreply.github.com>
2019-08-27 03:10:58 +0200

    Update WeakReferenceExtensions

--- src/Smaragd/Helpers/WeakReferenceExtensions.cs
+++ src/Smaragd/Helpers/WeakReferenceExtensions.cs
@@ -13,6 +13,7 @@ namespace NKristek.Smaragd.Helpers
         /// <typeparam name="T">The generic type of the <see cref="WeakReference{T}"/>.</typeparam>
         /// <param name="weakReference">The <see cref="WeakReference{T}"/> of which the target should be retrieved.</param>
         /// <returns>The target of the given <paramref name="weakReference"/> or the <see langword="default"/> value of <typeparamref name="T"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="weakReference"/> is <see langword="null"/>.</exception>
         public static T? TargetOrDefault<T>(this WeakReference<T> weakReference)
             where T : class
         {
