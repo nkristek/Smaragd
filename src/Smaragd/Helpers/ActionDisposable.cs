f0c21817ebe09c21db1cb74e795ca95dbcdc6d8f
Niclas Kristek <nkristek@users.noreply.github.com>
2019-08-27 03:11:51 +0200

    Update ActionDisposable

--- src/Smaragd/Helpers/ActionDisposable.cs
+++ src/Smaragd/Helpers/ActionDisposable.cs
@@ -9,9 +9,9 @@ namespace NKristek.Smaragd.Helpers
     internal sealed class ActionDisposable
         : Disposable
     {
-        private readonly Action _disposeManagedResourcesAction;
+        private readonly Action? _disposeManagedResourcesAction;
 
-        private readonly Action _disposeNativeResourcesAction;
+        private readonly Action? _disposeNativeResourcesAction;
 
         /// <inheritdoc />
         /// <summary>
@@ -19,7 +19,7 @@ namespace NKristek.Smaragd.Helpers
         /// </summary>
         /// <param name="disposeManagedResourcesAction">Action to dispose managed resources. A managed resource is another managed type, which implements <see cref="IDisposable"/>.</param>
         /// <param name="disposeNativeResourcesAction">Action to dispose native resources. Native resources are anything outside the managed world such as native Windows handles etc.</param>
-        public ActionDisposable(Action disposeManagedResourcesAction, Action disposeNativeResourcesAction = null)
+        public ActionDisposable(Action? disposeManagedResourcesAction, Action? disposeNativeResourcesAction = null)
         {
             _disposeManagedResourcesAction = disposeManagedResourcesAction;
             _disposeNativeResourcesAction = disposeNativeResourcesAction;
